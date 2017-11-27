36c55cf3accab36ad44108b1c1ad06bf953b8082
blam92 <iraola.santi@gmail.com>
2017-11-27 12:30:03 -0800

    JSONStringify working :)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,24 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //for array
+  if (Array.isArray(obj)) {
+    var arrayObj = [];
+    for(var i = 0; i < obj.length; i++) {
+      arrayObj.push(stringifyJSON(obj[i]));
+    } 
+    return '[' + arrayObj.join(',') + ']';
+  } else if(obj && typeof(obj) === 'object') {
+    var objBin = [];
+    for (var key in obj) {
+      if (typeof(obj[key]) === 'function' || obj[key] === undefined) {
+        continue;
+      }
+      objBin.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + objBin.join(',') + '}';
+  } else if(typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+  return '' + obj;  
 };
