b3ac1dc2371f83abdf173176344f22664d2bca28
blam92 <iraola.santi@gmail.com>
2017-11-27 16:21:41 -0800

    Implement rough array and string parsing.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,70 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function(json) {
-  // your code goes here
+
+var GlobalCounter = function(json) {
+  this.index = 0;
+  this.main = json;
 };
+GlobalCounter.prototype.next = function() { this.index++; };
+GlobalCounter.prototype.prev = function() { this.index--; };
+GlobalCounter.prototype.curChar = function() { return this.main[this.index]; };
+GlobalCounter.prototype.nextChar = function() { 
+  this.next();
+  return this.curChar();
+};
+
+
+var parseJSON = function(json) {
+  var counter = new GlobalCounter(json);
+
+  var makeString = function() {
+    var string = counter.curChar();
+    if( string === '"') {
+      return "";
+    } else if(counter.nextChar() !== '"') {
+      string += makeString();
+    }  
+
+    return string;
+  };
+
+  var makeElement = function() {
+    var element = [];
+
+    element.push(identifier());
+
+    return element;
+  };
+
+  var makeArray = function() {
+    var result = [];
+    
+    if (counter.curChar() !== ']') {
+      result = result.concat(makeElement());
+    }
+
+    return result;
+  };
+
+
+  var identifier = function() {
+
+    if (counter.curChar() === '[') {
+      counter.next();
+      return makeArray();
+    } else if(counter.curChar() === '"') {
+      counter.next();
+      return makeString();
+    }
+  };
+
+
+  //identifier function to identify and return the correct datat type.
+  return identifier();
+  
+};
+
+var ownJSONTest = "[\"test\"]";
+
+console.log(parseJSON(ownJSONTest));
